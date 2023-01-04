19d7c14f02b288a63a425f9af07ec65974f45112
Ashwini22wh1a1211 <121919297+Ashwini22wh1a1211@users.noreply.github.com>
2023-01-04 12:19:13 +0530

    Add files via upload

---
+++ sample9.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+printf("\nNAME:keerthi");
+printf("\nAGE:20");
+printf("\nBLOOD GROUP:a+");
+printf("\nDATE OF BIRTH: 14.09.2003");
+printf("\nP:NO:6583756645");
+printf("\nADDRESS:pno:564,fno:1484,hyderabad,telangana.");
+printf("\nOCCUPATION:student");
+}
